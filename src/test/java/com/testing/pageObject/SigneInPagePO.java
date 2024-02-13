5a8a16964cc6105390b2aa48b54f6621599f5434
igbalaye <igbalayek@gmail.com>
2024-02-13 23:12:10 +0000

    AccountLogin

--- src/test/java/com/testing/pageObject/SigneInPagePO.java
+++ src/test/java/com/testing/pageObject/SigneInPagePO.java
@@ -1,8 +1,33 @@
 package com.testing.pageObject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SigneInPagePO {
 
+    // Element Locator
+    @FindBy(id = "email")
+    public static WebElement EmailField;
 
+
+    @FindBy(id = "login[password]")
+    public static WebElement PasswordField;
+
+    @FindBy(className = "customer-account-login page-layout-1column")
+    public static WebElement SignInButton;
+
+
+    // Initialize WebElement Using Selenium
+    public SigneInPagePO(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    // Method to describe web element action
+    public void enterEmail (String Email) {EmailField.sendKeys(Email);}
+    public void Password (String Password) {PasswordField.sendKeys(Password);}
+    public void ClickSignInButton () {SignInButton.click();}
 
 
 
--- target/test-classes/Feature/AccountManagement.feature
+++ target/test-classes/Feature/AccountManagement.feature
@@ -12,3 +12,20 @@ Feature: Account Management
     Examples:
       | Firstname | Lastname | Email         | Password    | ConfirmPassword |
       | Ara       | Zee      | Ara@gmail.com | Arazee99@20 | Arazee99@20     |
+
+
+
+
+  @SignIn
+  Scenario Outline: Create registered customer login using a valid email address
+    Given I am on signIn page
+    When I enter "<Email>" "<Password>"
+    And I click on sign in
+    Then My registered customer account page is displayed
+
+    Examples:
+      | Email          | Password      |
+      | Ara1@gmail.com | Arazee99@2020 |
+
+
+
